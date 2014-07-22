b4f0d29000c3507f4b21d98308823530b7454234
davesag <davesag@gmail.com>
2014-07-22 18:21:13 +1000

    updated packages

--- dist/jquery-myPlugin.1.0.0.js
+++ dist/jquery-myPlugin.1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * A jQuery plugin template written in Coffeescript - v1.0.0 - 2014-05-29
+ * A jQuery plugin template written in Coffeescript - v1.0.0 - 2014-07-22
  * https://github.com/davesag/coffeescript-jquery-plugin-template
  * Copyright (c) 2014 Dave Sag; Licensed MIT
  */
@@ -10,8 +10,7 @@
 
   (function($) {
     $.fn.myPlugin = function(options) {
-      var opts, self;
-      self = this;
+      var opts;
       opts = $.extend(true, {}, $.fn.myPlugin.options);
       this.options = typeof options === "object" ? $.extend(true, opts, options) : opts;
       return this.each(function() {
